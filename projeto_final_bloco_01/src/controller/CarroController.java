0584d32436490f9719ab863ae4cbc2e49133d6b1
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-13 11:48:57 -0300

    Create CarroController.java

---
+++ projeto_final_bloco_01/src/controller/CarroController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import repository.ICarroDAO;
+
+public class CarroController implements ICarroDAO {
+
+    @Override
+    public void criar() {
+
+    }
+
+    @Override
+    public void atualizar() {
+
+    }
+
+    @Override
+    public void deletar() {
+
+    }
+
+    @Override
+    public void listar() {
+
+    }
+}
