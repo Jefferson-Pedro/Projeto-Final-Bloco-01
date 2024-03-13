b9b01ebc3d664f2cd5aca3c9cb6bd5ee9ddaf960
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-13 11:44:09 -0300

    Create BuilderSedan.java
    - Criação da classe BuilderSedan;

---
+++ projeto_final_bloco_01/src/builder/BuilderSedan.java
@@ -0,0 +1,47 @@
+package builder;
+
+import model.CarroHatch;
+import model.CarroSedan;
+
+public class BuilderSedan {
+
+    private int id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int placa;
+    private int tamPortaMalas;
+
+    public BuilderSedan id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public BuilderSedan marca(String marca) {
+        this.marca = marca;
+        return this;
+    }
+
+    public BuilderSedan modelo(String modelo) {
+        this.modelo = modelo;
+        return this;
+    }
+
+    public BuilderSedan cor(String cor) {
+        this.cor = cor;
+        return this;
+    }
+
+    public BuilderSedan placa(int placa) {
+        this.placa = placa;
+        return this;
+    }
+
+    public BuilderSedan lugares(int tamPortaMalas) {
+        this.tamPortaMalas = tamPortaMalas;
+        return this;
+    }
+    public CarroSedan criaHatch (){
+        return  new CarroSedan(id, marca, modelo, cor, placa, tamPortaMalas);
+    }
+}
