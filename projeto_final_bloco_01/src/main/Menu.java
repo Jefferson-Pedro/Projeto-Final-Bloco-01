3c0cd2dc023cba924f70b84cb1c7a56a25562d90
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-13 13:20:32 -0300

    Update Menu.java
    - Atualização do Menu para organização e retorno dos objetos do controller;

--- projeto_final_bloco_01/src/main/Menu.java
+++ projeto_final_bloco_01/src/main/Menu.java
@@ -1,42 +1,29 @@
 package main;
 
+import controller.CarroController;
+import model.Carro;
 import util.Cores;
 
 import java.util.Scanner;
 
 public class Menu {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
 
-
+        CarroController controller = new CarroController();
         Scanner leia = new Scanner(System.in);
 
         int opcao;
 
-        while (true) {
+        exibirMenu();
 
-            System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
-                    + "*****************************************************");
-            System.out.println("                                                     ");
-            System.out.println("                LOCADORA MOTORISTA FELIZ             ");
-            System.out.println("                                                     ");
-            System.out.println("*****************************************************");
-            System.out.println("                                                     ");
-            System.out.println("            1 - Cadastrar Novo Carro                 ");
-            System.out.println("            2 - Listar todos os Carros               ");
-            System.out.println("            3 - Buscar Carro por ID                  ");
-            System.out.println("            4 - Atualizar Dados do Carro             ");
-            System.out.println("            5 - Apagar Carro                         ");
-            System.out.println("            0 - Sair                                 ");
-            System.out.println("                                                     ");
-            System.out.println("*****************************************************");
-            System.out.print("Entre com a opção desejada:");
-            System.out.print("        " + Cores.TEXT_RESET);
+        while (true) {
 
             opcao = leia.nextInt();
 
             if (opcao == 0) {
                 System.out.println(Cores.TEXT_WHITE_BOLD + "\nSeu sonho do seu novo carro começa aqui!");
                 System.out.println(Cores.TEXT_GREEN + "\nObrigado por usar este programa!");
+                sobre();
                 leia.close();
                 System.exit(0);
             }
@@ -44,6 +31,9 @@ public class Menu {
             switch (opcao) {
                 case 1:
                     System.out.println(Cores.TEXT_WHITE + "Cadastrar Novo Carro \n\n");
+                    Carro carroCriado = controller.criar();
+                    carroCriado.VisualizarCarro();
+                    exibirMenu();
                     break;
                 case 2:
                     System.out.println(Cores.TEXT_WHITE + "Listar todos os Carros \n\n");
@@ -63,9 +53,30 @@ public class Menu {
                     break;
                 default:
                     System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+                    exibirMenu();
                     break;
             }
         }
+    }
+
+    public static void exibirMenu(){
+        System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+                + "*****************************************************");
+        System.out.println("                                                     ");
+        System.out.println("                LOCADORA MOTORISTA FELIZ             ");
+        System.out.println("                                                     ");
+        System.out.println("*****************************************************");
+        System.out.println("                                                     ");
+        System.out.println("            1 - Cadastrar Novo Carro                 ");
+        System.out.println("            2 - Listar todos os Carros               ");
+        System.out.println("            3 - Buscar Carro por ID                  ");
+        System.out.println("            4 - Atualizar Dados do Carro             ");
+        System.out.println("            5 - Apagar Carro                         ");
+        System.out.println("            0 - Sair                                 ");
+        System.out.println("                                                     ");
+        System.out.println("*****************************************************");
+        System.out.print("Entre com a opção desejada:");
+        System.out.print("        " + Cores.TEXT_RESET);
     }
 
     public static void sobre() {
