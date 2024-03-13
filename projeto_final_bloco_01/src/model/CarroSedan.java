ef10a6f7b6af2957adf1bab21a2f337489d07e58
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-13 11:34:44 -0300

    Update CarroSedan.java

--- projeto_final_bloco_01/src/model/CarroSedan.java
+++ projeto_final_bloco_01/src/model/CarroSedan.java
@@ -8,4 +8,19 @@ public class CarroSedan extends Carro{
         super(id, marca, modelo, cor, placa);
         this.tamPortaMalas = tamPortaMalas;
     }
+
+    public int getTamPortaMalas() {
+        return tamPortaMalas;
+    }
+
+    public void setTamPortaMalas(int tamPortaMalas) {
+        this.tamPortaMalas = tamPortaMalas;
+    }
+
+    @Override
+    public void VisualizarCarro() {
+        super.VisualizarCarro();
+        System.out.println("Tipo de carro: SEDAN");
+        System.out.println("Tamanho do porta malas: " + this.tamPortaMalas);
+    }
 }
